df096412cf0f27f3c1ec0d64e98b892635483d3b
Đinh Quang Anh <56481830+dinhquanganh@users.noreply.github.com>
2020-05-18 13:57:26 +0700

    up2

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def profile():
 def login():
     return render_template('login.html')   
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')   
+
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
  
